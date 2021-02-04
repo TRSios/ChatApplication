dfe5ebe0f3fa178bfddad3ee68ba8a8ff17bbe52
Taras Havryliv <Taras.Havryliv@vitech.com.ua>
2021-02-04 21:13:59 +0200

    feat: added threads;

--- assets/js/application.js
+++ assets/js/application.js
@@ -22,5 +22,40 @@ for (let i = 0; i < channels.length; i++) {
         const currentActive = document.getElementsByClassName('active-channel-name');
         currentActive[0].className = currentActive[0].className.replace(' active-channel-name', '');
         this.className += ' active-channel-name';
+
+        const threads = document.querySelector('.threads');
+
+        if (this.innerText.includes('#designers')) {
+            threads.className = threads.className.replace(' hidden', '');
+        } else if (!threads.className.includes('hidden')) {
+            threads.className += ' hidden';
+        }
+    });
+}
+
+const threadsNavBar = document.querySelector('.thread-header .nav-bar');
+const threadsNavBarItems = threadsNavBar.getElementsByTagName('a');
+for (let i = 0; i < threadsNavBarItems.length; i++) {
+    threadsNavBarItems[i].addEventListener('click', function () {
+        const currentActive = threadsNavBar.getElementsByClassName('active-nav-bar');
+        currentActive[0].className = currentActive[0].className.replace('active-nav-bar', '');
+        this.className += 'active-nav-bar';
+
+        const threads = document.querySelector('.threads ul');
+
+        if (this.text === 'All messages') {
+            threads.className = threads.className.replace('hidden', '');
+        } else if (!threads.className.includes('hidden')) {
+            threads.className += 'hidden';
+        }
+    });
+}
+
+const threads = document.querySelectorAll('.thread');
+for (let i = 0; i < threads.length; i++) {
+    threads[i].addEventListener('click', function () {
+        const currentActive = document.getElementsByClassName('active-thread');
+        currentActive[0].className = currentActive[0].className.replace(' active-thread', '');
+        this.className += ' active-thread';
     });
 }
